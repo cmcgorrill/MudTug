f4e35b03269f0f547deb6f682ff7870e22061922
Courtney McGorrill <courtney@Courtneys-MacBook-Pro.local>
2022-01-24 19:36:46 -0500

    Boilerplate frontend to start List of team info -- not yet pulling data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const teams = [
+    {
+        id: 1,
+        name: "A Team",
+        captainName: "Alan",
+        bracket: "Male",
+    },
+    {
+        id: 2,
+        name: "B Team",
+        captainName: "Betty",
+        bracket: "Female",
+    },
+    {
+        id: 3,
+        name: "C Team",
+        captainName: "Connor",
+        bracket: "Coed",
+    },
+    {
+        id: 4,
+        name: "D Team",
+        captainName: "Danielle",
+        bracket: "Coed",
+    },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      teamList: teams,
+    };
+  }
+
+  displayCompleted = (status) => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+
+    return this.setState({ viewCompleted: false });
+  };
+
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+
+    return this.state.teamList.map((team) => (
+      <li
+        key={team.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span>{team.name}</span>
+        <span>
+          <button
+            className="btn btn-secondary mr-2"
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </span>
+      </li>
+    ));
+  };
+
+  render() {
+    return (
+      <main className="container">
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="mb-4">
+                <button
+                  className="btn btn-primary"
+                >
+                  Add team
+                </button>
+              </div>
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
